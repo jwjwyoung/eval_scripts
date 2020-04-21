4636fba94c333af042a2942fd668e3c361939570
jw-macpro <wsklxsx@126.com>
2020-04-21 12:55:29 -0500

    update password

--- spree/eval_addlimit.rb
+++ spree/eval_addlimit.rb
@@ -3,7 +3,7 @@ load File.join(File.dirname(__FILE__),  '../eval_mysql.rb')
 
 # initialize the conn
 db = "spree"
-conn = PG.connect(:hostaddr => "127.0.0.1", :port => 5432, :dbname => db, :user => "jwy", :password => "")
+conn = PG.connect(:hostaddr => "127.0.0.1", :port => 5432, :dbname => db, :user => "junwen", :password => "1234")
 # mysql_conn = build_connection(db)
 
 
