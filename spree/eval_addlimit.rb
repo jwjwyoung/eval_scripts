588c14a8538feb30f84eabb70c62484790f15921
jw-macpro <wsklxsx@126.com>
2020-04-21 12:53:49 -0500

    add limit 1

---
+++ spree/eval_addlimit.rb
@@ -0,0 +1,9 @@
+load File.join(File.dirname(__FILE__),  '../eval.rb')
+load File.join(File.dirname(__FILE__),  '../eval_mysql.rb')
+
+# initialize the conn
+db = "spree"
+conn = PG.connect(:hostaddr => "127.0.0.1", :port => 5432, :dbname => db, :user => "jwy", :password => "")
+# mysql_conn = build_connection(db)
+
+
